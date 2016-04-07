5855ad9b65711553f0174813a34ebaac8cbe039b
Y. Z. Chen <754097087@qq.com>
2016-04-07 23:54:14 +0800

    [update] Finish base.

--- app/src/main/java/com/geno/nativerunner/MainActivity.java
+++ app/src/main/java/com/geno/nativerunner/MainActivity.java
@@ -1,34 +1,60 @@
 package com.geno.nativerunner;
 
 import android.app.*;
-import android.os.*;
+import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 import android.content.*;
 import android.view.View.*;
+import java.io.*;
 
 public class MainActivity extends Activity 
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
         super.onCreate(savedInstanceState);
-		LinearLayout l = new LinearLayout(this);
-		l.setOrientation(LinearLayout.VERTICAL);
-		setContentView(l);
-		addCommand(l, "ls");
-    }
+		LinearLayout total = new LinearLayout(this);
+		LinearLayout internal = new LinearLayout(this);
+		LinearLayout busybox = new LinearLayout(this);
+		LinearLayout external = new LinearLayout(this);
+		total.setOrientation(LinearLayout.VERTICAL);
+		internal.setOrientation(LinearLayout.VERTICAL);
+		busybox.setOrientation(LinearLayout.VERTICAL);
+		external.setOrientation(LinearLayout.VERTICAL);
+		TextView internalD = new TextView(this);
+		TextView busyboxD = new TextView(this);
+		TextView externalD = new TextView(this);
+		internalD.setText("Internal");
+		busyboxD.setText("Busybox");
+		externalD.setText("External");
+		internalD.setTextSize(20);
+		busyboxD.setTextSize(20);
+		externalD.setTextSize(20);
+		internalD.setGravity(Gravity.CENTER);
+		busyboxD.setGravity(Gravity.CENTER);
+		externalD.setGravity(Gravity.CENTER);
+		internal.addView(internalD, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		busybox.addView(busyboxD, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		external.addView(externalD, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		total.addView(internal, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		total.addView(busybox, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		total.addView(external, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		setContentView(total);
+		addCommand(busybox, "ls", "busybox");
+	}
 	
-	public void addCommand(ViewGroup toAdd, final String command)
+	public void addCommand(ViewGroup toAdd, final String command, final String cmdclazz)
 	{
 		Button b = new Button(this);
 		b.setText(command);
+		b.setAllCaps(false);
 		b.setOnClickListener(new OnClickListener()
 			{
 				@Override
 				public void onClick(View p1)
 				{
-					startActivity(new Intent(MainActivity.this, Command.class).putExtra("command", command));
+					startActivity(new Intent(MainActivity.this, Command.class).putExtra("command", cmdclazz + " " + command));
 				}
 			});
 		b.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
